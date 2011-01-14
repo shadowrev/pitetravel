c9cd92d5deaa84fe5256a2d72057f5689797c3cd
shadowrev <shadowrev@0a1a8a1b-77bb-720d-6164-e147281eb201>
2011-01-14 20:48:23 +0000

    - Avanzando lista de contactos

--- trunk/apps/piteadmin/modules/cliente/templates/almacenarContactoSuccess.php
+++ trunk/apps/piteadmin/modules/cliente/templates/almacenarContactoSuccess.php
@@ -1,3 +1,4 @@
+<?php echo $form_contacto['con_codigo']->render() ?>
 <?php echo $form_contacto['con_nombre']->render() ?>
 <?php echo $form_contacto['con_telefono1']->render() ?>
 <?php echo $form_contacto['con_telefono2']->render() ?>
