0c639990c625a621a3bfdca4d44815d4c81618ff
shadowrev <shadowrev@0a1a8a1b-77bb-720d-6164-e147281eb201>
2011-01-16 16:46:17 +0000

    - Mensaje de advertencia al acceder a reserva de vuelo y de hotel

--- apps/piteadmin/modules/cliente/templates/reservaSuccess.php
+++ apps/piteadmin/modules/cliente/templates/reservaSuccess.php
@@ -1,6 +1,11 @@
 <?php if(strcmp($sf_context->getUser()->getAttribute('pac_nombre'), '') != 0): ?>
-<h2>Cliente actual:<em><?php echo $sf_context->getUser()->getAttribute('pac_nombre') ?></em></h2>
+<h2>Cliente actual:<em><?php echo $sf_context->getUser()->getAttribute('pac_nombre'); ?></em></h2>
+
+<?php if($sf_context->getUser()->getFlash('mensaje_advertencia')): ?>
+<div class="mensaje"><?php echo $sf_context->getUser()->getFlash('mensaje_advertencia'); ?></div>
 <?php endif; ?>
+<?php endif; ?>
+
 <?php include_partial('submenu', array('current' => 'hospedaje')) ?>
 <div id="contenido-formulario">
 <?php include_partial('formulario_hotel', array('form' => $form)) ?>
--- apps/piteadmin/modules/cliente/templates/vueloSuccess.php
+++ apps/piteadmin/modules/cliente/templates/vueloSuccess.php
@@ -1,5 +1,8 @@
 <?php if(strcmp($sf_context->getUser()->getAttribute('pac_nombre'), '') != 0): ?>
 <h2>Cliente actual:<em><?php echo $sf_context->getUser()->getAttribute('pac_nombre') ?></em></h2>
+<?php if($sf_context->getUser()->getFlash('mensaje_advertencia')): ?>
+<div class="mensaje"><?php echo $sf_context->getUser()->getFlash('mensaje_advertencia') ?></div>
+<?php endif; ?>
 <?php endif; ?>
 <?php include_partial('submenu', array('current' => 'vuelo')) ?>
 <div id="contenido-formulario">
