5ca8e8dfdfd93833d612bc3cbc9b90053bc37792
shadowrev <shadowrev@0a1a8a1b-77bb-720d-6164-e147281eb201>
2011-02-10 03:09:09 +0000

    - Control de acceso a los usuarios.

--- trunk/apps/piteadmin/templates/_tabs.php
+++ trunk/apps/piteadmin/templates/_tabs.php
@@ -7,33 +7,33 @@
 ?>
 <ul>
     <!-- CSS Tabs -->
-    <?php //if(in_array('cliente', $show)) : ?>
-    <li<?php echo (strcmp($current, 'cliente') == 0) ? ' class="current"' : '' ?>>
+    <?php //$enlace_cli = $sf_context->getUser()->hasCredential(array('medicos_locales', 'medicos_internacionales'), false) ? url_for('valoracion/examenesPreoperatorios') : '#' ?>
+    <?php //$pestanha_cli = ($enlace_cli == "#") ? 'desactivada' : '' ?>
+    <li class="<?php echo (strcmp($current, 'cliente') == 0) ? 'current' : '' ?>">
         <a href="<?php echo url_for('cliente/informacionPaciente') ?>"><span>Cliente</span></a>
     </li>
-    <?php //endif; ?>
-    <?php //if(in_array('valoracion', $show)) : ?>
-    <li<?php echo (strcmp($current, 'valoracion') == 0) ? ' class="current"' : '' ?>>
-        <a href="<?php echo url_for('valoracion/examenesPreoperatorios') ?>"><span>Valoraci&oacute;n</span></a>
+    <?php $enlace_val = $sf_context->getUser()->hasCredential(array('medicos_locales', 'medicos_internacionales'), false) ? url_for('valoracion/examenesPreoperatorios') : '#' ?>
+    <?php $pestanha_val = ($enlace_val == "#") ? 'desactivada' : '' ?>
+    <li class="<?php echo ((strcmp($current, 'valoracion') == 0) ? 'current' : '') . $pestanha_val ?>">
+        <a href="<?php echo $enlace_val ?>"><span>Valoraci&oacute;n</span></a>
     </li>
-    <?php //endif; ?>
-    <?php //if(in_array('alta', $show)) : ?>
-    <li<?php echo (strcmp($current, 'postoperatorio') == 0) ? ' class="current"' : '' ?>>
+    <?php //$enlace_pos = $sf_context->getUser()->hasCredential(array('medicos_locales', 'medicos_internacionales'), false) ? url_for('valoracion/examenesPreoperatorios') : '#' ?>
+    <?php //$pestanha_pos = ($enlace_pos == "#") ? 'desactivada' : '' ?>
+    <li class="<?php echo ((strcmp($current, 'postoperatorio') == 0) ? 'current' : '') ?>">
         <a href="<?php echo url_for('postoperatorio/infoPostOperatorio') ?>"><span>Post-Operatorio</span></a>
     </li>
-    <?php //endif; ?>
-    <?php //if(in_array('logistica', $show)) : ?>
-    <li<?php echo (strcmp($current, 'logistica') == 0) ? ' class="current"' : '' ?>>
+    <?php //$enlace_log = $sf_context->getUser()->hasCredential(array('medicos_locales', 'medicos_internacionales'), false) ? url_for('valoracion/examenesPreoperatorios') : '#' ?>
+    <?php //$pestanha_log = ($enlace_log == "#") ? 'desactivada' : '' ?>
+    <li class="<?php echo ((strcmp($current, 'logistica') == 0) ? 'current' : '') ?>">
         <a href="<?php echo url_for('logistica/logistica') ?>"><span>Log&iacute;stica</span></a>
     </li>
-    <?php //endif; ?>
-    <li<?php echo (strcmp($current, 'informes') == 0) ? ' class="current"' : '' ?>>
-        <a href="#"><span>Reportes</span></a>
+    <li class="<?php echo ((strcmp($current, 'reportes') == 0) ? 'current' : '') ?>">
+        <a href="<?php echo url_for('reportes/generarReporteLogistica') ?>"><span>Reportes</span></a>
     </li>
-    <?php //if(in_array('usuarios', $show)) : ?>
-    <li<?php echo (strcmp($current, 'sfGuardUser') == 0) ? ' class="current"' : '' ?>>
-        <a href="<?php echo url_for('@sf_guard_user') ?>"><span>Administraci&oacute;n de Usuarios</span></a>
+    <?php $enlace_adm = $sf_context->getUser()->hasCredential('admin') ? url_for('@sf_guard_user') : '#' ?>
+    <?php $pestanha_adm = ($enlace_adm == "#") ? 'desactivada' : '' ?>
+    <li class="<?php echo ((strcmp($current, 'sfGuardUser') == 0) ? 'current' : '') . $pestanha_adm ?>">
+        <a href="<?php echo $enlace_adm ?>"><span>Administraci&oacute;n de Usuarios</span></a>
     </li>
-    <?php //endif; ?>
     <li class="logout"><a href="<?php echo url_for('@sf_guard_signout') ?>"><span>Logout</span></a></li>
 </ul>
