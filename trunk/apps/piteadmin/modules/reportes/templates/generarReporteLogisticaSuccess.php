79a13d1c978fba18109aaed7a2b9cc20a8db4860
shadowrev <shadowrev@0a1a8a1b-77bb-720d-6164-e147281eb201>
2011-02-10 22:18:50 +0000

    ---

--- trunk/apps/piteadmin/modules/reportes/templates/generarReporteLogisticaSuccess.php
+++ trunk/apps/piteadmin/modules/reportes/templates/generarReporteLogisticaSuccess.php
@@ -3,11 +3,16 @@
     <div class="formulario">
         <h2>Informe de Logistica</h2>
         <div class="form">
-            <?php include_partial('reporte_logistica', array()) ?>
+            <?php include_partial('reporte_logistica', array(
+                'paciente' => $paciente,
+                'tratamiento' => $tratamiento
+            )) ?>
+            <?php if($paciente): ?>
             <div class="submit">
                 <button type="button">Guardar PDF</button>
                 <button type="button">Enviar por E-Mail</button>
             </div>
+            <?php endif ?>
         </div>
     </div>
 </div>
