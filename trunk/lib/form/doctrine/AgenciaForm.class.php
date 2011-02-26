ab612c17bda6bda06a6d91933275c7ba0eebf8cf
shadowrev <shadowrev@0a1a8a1b-77bb-720d-6164-e147281eb201>
2011-02-26 22:30:55 +0000

    - Agregado el control por agencias de turismo

---
+++ trunk/lib/form/doctrine/AgenciaForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Agencia form.
+ *
+ * @package    pitetravel
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class AgenciaForm extends BaseAgenciaForm
+{
+  public function configure()
+  {
+  }
+}
