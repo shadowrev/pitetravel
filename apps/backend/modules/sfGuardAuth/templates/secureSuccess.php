288789f8745b8a1717328f98bf9bdc953ffe5aab
shadowrev <shadowrev@0a1a8a1b-77bb-720d-6164-e147281eb201>
2011-03-02 03:28:07 +0000

    - sfGuardAuth para proteger el backend

---
+++ apps/backend/modules/sfGuardAuth/templates/secureSuccess.php
@@ -0,0 +1,9 @@
+<div id="login">
+  <h2>Acceso Restringido</h2>
+  <div class="alert">
+      Para Acceder a esta secci&oacute;n, debe ingresar como un Administrador.
+  </div>
+  <div class="submit">
+      <button type="button" onclick="location.href = '<?php echo url_for('@sf_guard_signout') ?>'">Cerrar Sesi&oacute;n</button>
+  </div>
+</div>
