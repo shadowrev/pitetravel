910ebb93f42f122457100439040e1b4fd9b86f24
shadowrev <shadowrev@0a1a8a1b-77bb-720d-6164-e147281eb201>
2011-03-21 20:10:12 +0000

    - Correcciones de mantenimiento - Elimina pacientes y contactos de pacientes

--- apps/piteadmin/modules/cliente/templates/_lista_contactos.php
+++ apps/piteadmin/modules/cliente/templates/_lista_contactos.php
@@ -5,6 +5,8 @@
     <td><?php echo $contacto_paciente->con_telefono1 ?></td>
     <td><?php echo $contacto_paciente->con_email ?></td>
     <td><a href="javascript:cargarContacto('<?php echo $contacto_paciente->con_codigo ?>')">[modificar]</a></td>
-    <td><a href="javascript:eliminarContacto('<?php echo $contacto_paciente->con_codigo ?>')">[eliminar]</a></td><?php $par = !$par ?>
+    <!--<td><a href="javascript:eliminarContacto('<?php echo $contacto_paciente->con_codigo ?>')">[eliminar]</a></td>-->
+    <td><?php echo link_to('[eliminar]', 'cliente/eliminarContacto?con_codigo=' . $contacto_paciente->con_codigo, array('confirm' => 'Está seguro?')) ?></td>
+    <?php $par = !$par ?>
 </tr>
 <?php endforeach; ?>
