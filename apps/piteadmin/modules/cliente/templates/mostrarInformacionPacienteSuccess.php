46c2a3774956bbc9d1f89484efe1cb31b2e800fd
shadowrev <shadowrev@0a1a8a1b-77bb-720d-6164-e147281eb201>
2011-05-07 20:53:12 +0000

    - Corregido error al crear un nuevo paciente. Detectado en el envío de correos - Inicio de la implementación de validadores en JQuery

--- apps/piteadmin/modules/cliente/templates/mostrarInformacionPacienteSuccess.php
+++ apps/piteadmin/modules/cliente/templates/mostrarInformacionPacienteSuccess.php
@@ -1,6 +1,10 @@
+<?php use_helper('JavascriptBase') ?>
 <?php if(strcmp($sf_context->getUser()->getAttribute('pac_nombre'), '') != 0): ?>
 <h2>Cliente actual:<em><?php echo $sf_context->getUser()->getAttribute('pac_nombre') ?></em></h2>
 <?php endif; ?>
+<?php echo javascript_tag('$().ready(function() {
+        $("#form1").validate();
+    });') ?>
 <?php include_partial('submenu', array('current' => 'datos')) ?>
 <div id="contenido-formulario">
 <?php include_partial('formulario_cliente', array(
