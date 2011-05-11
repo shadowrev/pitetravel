d03d30c94fdb56a47e49173c4f22dcfa3cb9702a
shadowrev <shadowrev@0a1a8a1b-77bb-720d-6164-e147281eb201>
2011-05-11 01:09:15 +0000

    - Validadores para todo el modulo de cliente

--- trunk/apps/piteadmin/modules/cliente/templates/reservaSuccess.php
+++ trunk/apps/piteadmin/modules/cliente/templates/reservaSuccess.php
@@ -1,3 +1,4 @@
+<?php use_helper('JavascriptBase') ?>
 <?php if(strcmp($sf_context->getUser()->getAttribute('pac_nombre'), '') != 0): ?>
 <h2>Cliente actual:<em><?php echo $sf_context->getUser()->getAttribute('pac_nombre'); ?></em></h2>
 
@@ -5,7 +6,9 @@
 <div class="mensaje"><?php echo $sf_context->getUser()->getFlash('mensaje_advertencia'); ?></div>
 <?php endif; ?>
 <?php endif; ?>
-
+<?php echo javascript_tag('$().ready(function() {
+        $("#form1").validate();
+    });') ?>
 <?php include_partial('submenu', array('current' => 'hospedaje')) ?>
 <div id="contenido-formulario">
 <?php include_partial('formulario_hotel', array('form' => $form)) ?>
--- trunk/apps/piteadmin/modules/cliente/templates/vueloSuccess.php
+++ trunk/apps/piteadmin/modules/cliente/templates/vueloSuccess.php
@@ -1,9 +1,13 @@
+<?php use_helper('JavascriptBase') ?>
 <?php if(strcmp($sf_context->getUser()->getAttribute('pac_nombre'), '') != 0): ?>
 <h2>Cliente actual:<em><?php echo $sf_context->getUser()->getAttribute('pac_nombre') ?></em></h2>
 <?php if($sf_context->getUser()->getFlash('mensaje_advertencia')): ?>
 <div class="mensaje"><?php echo $sf_context->getUser()->getFlash('mensaje_advertencia') ?></div>
 <?php endif; ?>
 <?php endif; ?>
+<?php echo javascript_tag('$().ready(function() {
+        $("#form1").validate();
+    });') ?>
 <?php include_partial('submenu', array('current' => 'vuelo')) ?>
 <div id="contenido-formulario">
 <?php include_partial('formulario_vuelo', array('form' => $form)) ?>
